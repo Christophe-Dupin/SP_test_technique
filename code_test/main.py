d73b91e48f7b2ed1572064a5871b6a97b22c7236
christophe-dupin <cdupin@equancy.com>
2020-11-27 21:47:22 +0100

    remove unused import

--- code_test/main.py
+++ code_test/main.py
@@ -6,14 +6,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import *  # noqa: I201,F4
-import yaml
-import re
-import os
-import shutil
-import errno
 from code_test import LOG
-from  code_test.publish.utils import PublishTools
-from code_test.config import TEX_ASSIGN_FILE, ASSET_NAME, TASK, EXTENSION, ROOT_PROJECT, WORK_PATH, PUBLISH_PATH, CONTEXT
+from code_test.publish.utils import PublishTools
+from code_test.config import ASSET_NAME, TASK, EXTENSION, ROOT_PROJECT, CONTEXT
 
 
 def publish_texture():
@@ -28,12 +23,17 @@ def publish_texture():
     :return: A resume of the publish, like {"published": [], "already-published": [], "failed": []}
     :rtype: dict
     """
-    # Init result
+    # Instanciate PublishTools Class
     publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
+    # Get work directory of the texturefile for a specific asset
     get_work_file_directory = publish_tools.get_work_file_directory_from_asset()
+    # Get path of of the texture assignement yaml file
     get_texture_assignement_yaml = publish_tools.get_texture_assignement_yaml_file(get_work_file_directory)
+    # Get use textures in the yaml file
     get_use_textures_from_yaml = publish_tools.get_use_textures_from_yaml(get_texture_assignement_yaml)
+    # Keep only shot name of the textures files
     get_only_files_name = publish_tools.get_only_files_name(get_use_textures_from_yaml)
+    # Copy selected textures files from work directory to publish directory and rename with publish nomenclatura
     publish_tools.move_and_rename_file(get_work_file_directory, get_only_files_name)
 
 
@@ -45,5 +45,5 @@ def publish_texture():
 
 
 if __name__ == "__main__":
-    publish_texture()
+    a =publish_texture ()
 
