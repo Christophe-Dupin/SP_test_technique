860d52b03d7b9a5f4d887f756c30df080058bb48
christophe-dupin <cdupin@equancy.com>
2020-11-27 16:58:26 +0100

    continue to write test for publishTools CLass

--- test/test_publish_tools.py
+++ test/test_publish_tools.py
@@ -1,22 +1,24 @@
 """Class allow to test methodes from PublishTools Class."""
 import os 
 
+import yaml
 from code_test.publish.utils import PublishTools
-from code_test.config import ASSET_NAME, TASK, ROOT_PROJECT, CONTEXT,TEX_ASSIGN_FILE
+from code_test.config import ASSET_NAME, TASK, ROOT_PROJECT, CONTEXT, TEX_ASSIGN_FILE, EXTENSION
 
 
 class TestPublishTools:
 
     def test_get_work_file_directory_from_asset(self):
-        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK)
+        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
         path = publish_tools.get_work_file_directory_from_asset()
         assert path == "/Users/christophedupin/Documents/projects/repos/testSuperprod/code_test_a_cdupin/project/Assets/myAssetB/surfacing/work/"
     
     def test_get_texure_assignement_yaml_file(self):
-        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK)
+        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
         file = publish_tools.get_texture_assignement_yaml_file()
         assert file
 
-    def test_use_texture_from_yaml(self):
-        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK)
-        yaml = publish.get_texture_assignement_yaml_file(TEX_ASSIGN_FILE)
+    def test_get_use_textures_from_yam(self):
+        publish_tools = PublishTools(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
+        file = publish_tools.get_use_textures_from_yaml(TEX_ASSIGN_FILE)
+        assert isinstance(file, dict)
