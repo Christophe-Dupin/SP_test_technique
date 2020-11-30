1fc188d808eb4368f7df0ee4414e2aaafbef5783
christophe-dupin <cdupin@equancy.com>
2020-11-30 23:07:08 +0100

    modify main file to add publish tools

--- test/test_main.py
+++ test/test_main.py
@@ -9,8 +9,7 @@ from builtins import *  # noqa: I201,F4
 import pytest
 
 from code_test import LOG
-from code_test import main as ctm
-
+from code_test.main import PublishTools
 
 def test_publish_texture():
     """Simple test for the publish_texture.
@@ -18,6 +17,7 @@ def test_publish_texture():
     This test show you, how we basically test the publish texture function.
     Of course we don't compare the result in this test in order not to influence the code test.
     """
+    publish_tools = PublishTools()
     LOG.info(
         "Test : asset_name={asset_name}, task={task}, tex_assign_file={tex_assign_file}".format(
             asset_name=pytest.ASSET_NAME,
@@ -25,4 +25,4 @@ def test_publish_texture():
             tex_assign_file=pytest.TEX_ASSIGN_FILE,
         )
     )
-    ctm.publish_texture()
+    publish_tools.publish_texture()
