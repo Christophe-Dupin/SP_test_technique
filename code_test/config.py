9c58a96f7fd265ac51f99603f127e267fc0b3487
christophe-dupin <cdupin@equancy.com>
2020-11-30 22:02:21 +0100

    add constant

--- code_test/config.py
+++ code_test/config.py
@@ -1,3 +1,5 @@
+    """All constants for the publish tools class."""
+    
 import os
 
 CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -9,5 +11,6 @@ CONTEXT = "Assets"
 EXTENSION = "tx"
 ASSIGNMENT_FILE = "texture_assignment.yaml" 
 TEX_ASSIGN_FILE = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "work", "texture_assignment.yaml")
+TEX_PUBLISH_ASSIGN_FILE = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "publish", "texture_assignment.yaml")
 WORK_PATH = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "work/")
 PUBLISH_PATH = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "publish/")
