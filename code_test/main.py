9e5f7211a4764c4db19616fd0a13484c3877d53b
christophe-dupin <cdupin@equancy.com>
2020-11-30 22:02:57 +0100

    create a main PublishTools class

--- code_test/main.py
+++ code_test/main.py
@@ -7,41 +7,37 @@ from __future__ import (absolute_import, division, print_function,
 from builtins import *  # noqa: I201,F
 
 from code_test.config import ASSET_NAME, ASSIGNMENT_FILE, CONTEXT, EXTENSION, ROOT_PROJECT, TASK
-from code_test.publish.utils import FilesManager, YamlManager
+from code_test.publish.tools import FilesManager, YamlManager
 
 
-def publish_texture():
-    """Publish the textures defined by the tex_assign_file.
+class PublishTools:
+    def publish_texture(self):
+        """Publish the textures defined by the tex_assign_file."""
 
-    :param asset_name: Asset name like "myAssetB"
-    :type asset_name: str
-    :param task: Task name like "surfacing"
-    :type task: str
-    :param tex_assign_file: Path to the texture_assignment.yaml file
-    :type tex_assign_file: str
-    :return: A resume of the publish, like {"published": [], "already-published": [], "failed": []}
-    :rtype: dict
-    """
-    # Instanciate FilesManager class and YamlManager class
-    files_manager = FilesManager(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
-    yaml_manager = YamlManager(ASSIGNMENT_FILE)
-    # Get work directory of the texturefile for a specific asset
-    get_work_file_directory = files_manager.get_work_file_directory_from_asset()
-    # Get path of of the texture assignement yaml file
-    get_texture_assignement_yaml = yaml_manager.get_texture_assignement_yaml_file(get_work_file_directory)
-    # Get use textures in the yaml file
-    get_use_textures_from_yaml = yaml_manager.get_use_textures_from_yaml(get_texture_assignement_yaml)
-    # Keep only shot name of the textures files
-    get_only_files_name = files_manager.get_only_files_name(get_use_textures_from_yaml)
-    # Copy selected textures files from work directory to publish directory and rename with publish nomenclatura
-    files_manager.move_and_rename_file(get_work_file_directory, get_only_files_name)
-
-    # result = {"published": [], "already-published": [], "failed": []}
-
-    # LOG.info("result : {}".format(result))
-
-    # return result
-
+        # Instanciate FilesManager class
+        files_manager = FilesManager(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, EXTENSION)
+        # Instanciate YamlManager class
+        yaml_manager = YamlManager(ASSIGNMENT_FILE)
+        # Get work directory for a specific asset
+        asset_work_directory = files_manager.get_file_directory_from_asset(files_manager.work)
+        # Get publish directory for a specific asset
+        asset_publish_directory = files_manager.get_file_directory_from_asset(files_manager.publish)
+        # Get path of of the work texture assignement yaml file
+        yaml_work_file = yaml_manager.get_yaml_file(asset_work_directory)
+        # Get path of of the publish texture assignement yaml file
+        yaml_publish_file = yaml_manager.get_yaml_file(asset_publish_directory)
+        # Get used textures in the work yaml file
+        info_from_yaml_work_file = yaml_manager.get_info_from_yaml(yaml_work_file)
+        # Keep only short name of the textures files
+        only_files_name = files_manager.get_only_files_name(info_from_yaml_work_file)
+        # Get texture which are already publish
+        info_from_yaml_publish_file = yaml_manager.get_info_from_yaml(yaml_publish_file)
+        # Copy selected textures files from work directory to publish directory and rename with publish nomenclatura
+        files_manager.move_and_rename_file(asset_work_directory, only_files_name, info_from_yaml_publish_file)
+        # Update yaml with the published texture
+        publish_yaml_file = yaml_manager.update_yaml(yaml_publish_file, only_files_name)
 
 if __name__ == "__main__":
-    a = publish_texture()
+    publish_tools = PublishTools()
+    publish_tools.publish_texture()
+
