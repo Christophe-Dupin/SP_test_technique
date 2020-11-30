ea8166f493aa2af02b4645244d49b3131de589b8
christophe-dupin <cdupin@equancy.com>
2020-11-30 15:02:39 +0100

    Fix PEP8 constant add Assignement File constant

--- code_test/config.py
+++ code_test/config.py
@@ -1,12 +1,13 @@
 import os
 
-current_folder = os.path.dirname(os.path.abspath(__file__))
-root_repository = os.path.dirname(current_folder)
-ROOT_PROJECT = os.path.join(root_repository, "project")
+CURRENT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+ROOT_REPOSITORY = os.path.dirname(CURRENT_FOLDER)
+ROOT_PROJECT = os.path.join(ROOT_REPOSITORY, "project")
 ASSET_NAME = "myAssetB"
 TASK = "surfacing"
 CONTEXT = "Assets"
 EXTENSION = "tx"
+ASSIGNMENT_FILE = "texture_assignment.yaml" 
 TEX_ASSIGN_FILE = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "work", "texture_assignment.yaml")
 WORK_PATH = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "work/")
 PUBLISH_PATH = os.path.join(ROOT_PROJECT, CONTEXT, ASSET_NAME, TASK, "publish/")
