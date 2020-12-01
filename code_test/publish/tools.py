f6c38373044742772c5a242716b31d26c6db6119
christophe-dupin <cdupin@equancy.com>
2020-12-01 10:33:14 +0100

    Pep8

--- code_test/publish/tools.py
+++ code_test/publish/tools.py
@@ -11,20 +11,18 @@ from code_test import LOG
 
 
 class YamlManager:
-    """Library of tools for publish."""
+    """Library for managing yaml file."""
 
     def __init__(self, assignment):
-        """PublishTools is a class for toolbox to publish texture file.
+        """Allow to mange yaml file.
 
-        :param asset_name: Asset name like "myAssetB"
-        :type asset_name: str
-        :param task: Task name like "surfacing"
-        :type task: str
+        :param assignment: name of the yaml file
+        :type assignment: str
         """
         self.assignment = assignment
 
     def get_yaml_file(self, location):
-        """Methode to check if the yaml file exist in the good location.
+        """Get the yaml file directory.
 
         :param location: path of the work folder of the asset use get_work_file_directory method.
         :type location: PublishTools object
@@ -38,18 +36,26 @@ class YamlManager:
             LOG.error("yaml file doesnt exist please contact your td pipeline")
 
     def get_info_from_yaml(self, file):
-        """Methode to get all the texture assign.
+        """Get info from any yaml file
 
-        :param tex_assign_file: yaml file for the asset
-        :type tex_assign_file: yaml
-        :raises FileNotFoundError: Raise error if the file doesnt exist
-        :return: A dictionnary with the usefull texture
+        :param file: path of the yaml file
+        :type file: str
+        :return: Python dict of yaml content
         :rtype: dict
         """
         with open(file) as files:
             return yaml.load(files, Loader=yaml.FullLoader)
     
     def update_yaml(self, file_path, data):
+        """Update data in yaml file
+
+        :param file_path: file path of the yaml file
+        :type file_path: str
+        :param data: Dictionnairy of the data to update
+        :type data: dict
+        :return: new yaml file
+        :rtype: yaml
+        """
         with open(file_path, 'w') as file:
             return yaml.dump(data, file)
 
@@ -57,12 +63,18 @@ class YamlManager:
 class FilesManager:
 
     def __init__(self, root_project, context, asset_name, task, extension):
-        """PublishTools is a class for toolbox to publish texture file.
+        """Allow to acces to several tools to manage Files.
 
-        :param asset_name: Asset name like "myAssetB"
+        :param root_project: Define the path of the root project define in the config module
+        :type root_project: str
+        :param context: Define the context of the asset asset or shot for example
+        :type context: str
+        :param asset_name: Name of the asset for example myAssetB
         :type asset_name: str
-        :param task: Task name like "surfacing"
+        :param task: Define the task ofthe asset for example surfacing
         :type task: str
+        :param extension: Define the extension of the textexture file here its tx
+        :type extension: str
         """
         self.context = context
         self.root_project = root_project
@@ -74,10 +86,12 @@ class FilesManager:
         self.publish_path = os.path.join(self.root_project, self.context, self.asset_name, self.task, self.publish)
 
     def get_file_directory_from_asset(self, step):
-        """Methode to get the workFile directory of the asset.
+        """Get the file directory of any asset.
 
-        :raises FileNotFoundError: If direcotry dosn't exist raise an error.
-        :return: the path of the directory
+        :param step: You can choose work or publish directory
+        :type step: str
+        :raises FileNotFoundError: If the path diesnt exist raise an error
+        :return: return the path of the asset
         :rtype: str
         """
         path = os.path.join(self.root_project, self.context, self.asset_name, self.task, step)
@@ -87,12 +101,12 @@ class FilesManager:
         return path
 
     def get_only_files_name(self, textures):
-        """Methode to return only file texture file name.
+        """Get only the file name of any files, remove all the absolute path.
 
-        :param textures: textures files find inside the yaml
-        :type textures: dict
-        :return: only the name of the texture file without the full path
-        :rtype: str
+        :param textures: It's all the full path of a file 
+        :type textures: str
+        :return: dictionary of the file name
+        :rtype: dict
         """
         files_name = []
         for file in textures.values():
@@ -103,9 +117,9 @@ class FilesManager:
         return {"texture_publish": files_name}
 
     def set_name_for_publish_file(self, files):
-        """Set the correct publish file name for the texture file.
+        """Set publish file name for the texture file.
 
-        :param files: Should short namefor file texture
+        :param files: Should short name for file texture
         :type files: str
         :return: good texture name to respect nomenclatura
         :rtype: str
@@ -118,12 +132,16 @@ class FilesManager:
             LOG.error("Work File doesnt match the nomenclatura please check in the wiki")
     
     def move_and_rename_file(self, location, files, publish):
-        """Methode to all to move and rename file from work folder to the publish folder
+        """Move and rename file.
 
-        :param location: work directory of texture files
-        :type location: str
-        :param files: contain only the texture file name use in the scene
-        :type files: list
+        :param location: work directory is the source directory
+        :type location: YamlManager
+        :param files: shortt name of the files to move
+        :type files: FilesManager
+        :param publish: Get the already publish texture
+        :type publish: YamlManager
+        :return: Summary of what append in the method
+        :rtype: dict
         """
         result = {"published": [], "already-published": [], "failed": []}
         for x in files["texture_publish"]:
